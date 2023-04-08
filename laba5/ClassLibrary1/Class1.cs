4cccaf26eb50d9c34af1728884713a5f89735a15
den669 <71308629+den669@users.noreply.github.com>
2023-04-08 23:11:56 +0300

    Add files via upload

---
+++ laba5/ClassLibrary1/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+public class Suitcase : ICloneable
+{
+    public string Brand { get; set; }
+    public string Color { get; set; }
+    public int Size { get; set; }
+
+    public object Clone()
+    {
+        
+        return MemberwiseClone();
+    }
+
+    public override string ToString()
+    {
+        return $"Brand: {Brand}, Color: {Color}, Size: {Size}";
+    }
+}
+
+
+public class SuitcaseBuilder
+{
+    private Suitcase _suitcase;
+
+    public SuitcaseBuilder()
+    {
+        _suitcase = new Suitcase();
+    }
+
+    public SuitcaseBuilder SetBrand(string brand)
+    {
+        _suitcase.Brand = brand;
+        return this;
+    }
+
+    public SuitcaseBuilder SetColor(string color)
+    {
+        _suitcase.Color = color;
+        return this;
+    }
+
+    public SuitcaseBuilder SetSize(int size)
+    {
+        _suitcase.Size = size;
+        return this;
+    }
+
+    public Suitcase Build()
+    {
+        
+        return _suitcase;
+    }
+}
---
+++ laba5/laba5/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        
+        var builder = new SuitcaseBuilder();
+        var suitcase = builder.SetBrand("Samsonite")
+                              .SetColor("Black")
+                              .SetSize(24)
+                              .Build();
+        Console.WriteLine(suitcase);
+
+        var clonedSuitcase = (Suitcase)suitcase.Clone();
+        Console.WriteLine(clonedSuitcase);
+    }
+}
